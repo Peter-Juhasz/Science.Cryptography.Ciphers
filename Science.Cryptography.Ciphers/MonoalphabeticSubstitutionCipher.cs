bfd2310790baa1248ea8f11efe0497eb3125d6f0
peter-juhasz <peter.juhasz@live.com>
2015-01-15 22:48:18 +0100

    Monoalphabetic substitution

---
+++ Science.Cryptography.Ciphers/MonoalphabeticSubstitutionCipher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Science.Cryptography.Ciphers
+{
+    /// <summary>
+    /// Represents the Monoalphabetic Substitution cipher.
+    /// </summary>
+    public class MonoalphabeticSubstitutionCipher : IKeyedCipher<IReadOnlyDictionary<char, char>>
+    {
+        public string Encrypt(string plaintext, IReadOnlyDictionary<char, char> key)
+        {
+            char[] ciphertext = new char[plaintext.Length];
+
+            for (int i = 0; i < plaintext.Length; i++)
+            {
+                if (key.ContainsKey(Char.ToUpperInvariant(plaintext[i])))
+                    ciphertext[i] = key[plaintext[i]].ToSameCaseAs(plaintext[i]);
+                else
+                    ciphertext[i] = plaintext[i];
+            }
+
+            return new String(ciphertext);
+        }
+
+        public string Decrypt(string ciphertext, IReadOnlyDictionary<char, char> key)
+        {
+            char[] plaintext = new char[ciphertext.Length];
+
+            for (int i = 0; i < ciphertext.Length; i++)
+            {
+                if (key.Values.Contains(Char.ToUpperInvariant(ciphertext[i])))
+                    plaintext[i] = key[key.First(kv => kv.Value == Char.ToUpperInvariant(ciphertext[i])).Key].ToSameCaseAs(ciphertext[i]);
+                else
+                    plaintext[i] = ciphertext[i];
+            }
+
+            return new String(plaintext);
+        }
+    }
+}
