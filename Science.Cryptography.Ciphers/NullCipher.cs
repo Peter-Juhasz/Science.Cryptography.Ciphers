079aee27c9d859563c1020f2d7907cc855a21c35
peter-juhasz <peter.juhasz@live.com>
2015-01-17 16:48:43 +0100

    Null cipher

---
+++ Science.Cryptography.Ciphers/NullCipher.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Science.Cryptography.Ciphers
+{
+    /// <summary>
+    /// Represents the Null cipher.
+    /// </summary>
+    public class NullCipher : IKeyedCipher<IReadOnlyList<int>>
+    {
+        private readonly static Regex WordRegex = new Regex(@"\w+");
+
+        public string Encrypt(string plaintext, IReadOnlyList<int> key)
+        {
+            StringBuilder result = new StringBuilder();
+            int keyIndex = 0;
+
+            foreach (string word in this.GetWords(plaintext))
+            {
+                result.Append(word[key[keyIndex]]);
+
+                // advance key
+                keyIndex++;
+
+                if (keyIndex == key.Count)
+                    keyIndex = 0;
+            }
+
+            return result.ToString();
+        }
+
+        public string Decrypt(string ciphertext, IReadOnlyList<int> key)
+        {
+            throw new NotSupportedException();
+        }
+
+
+        protected IEnumerable<string> GetWords(string text)
+        {
+            return WordRegex.Matches(text).Cast<Match>().Select(m => m.Value);
+        }
+    }
+}
