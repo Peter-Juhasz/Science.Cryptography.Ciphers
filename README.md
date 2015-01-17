0f5e53113caa721762e176c248448f4f75bea957
peter-juhasz <peter.juhasz@live.com>
2015-01-17 18:12:04 +0100

    Frequency analysis

--- README.md
+++ README.md
@@ -5,11 +5,12 @@ This project collects classic ciphers implemented in .NET/C#.
 ## Ciphers
 Affine, Atbash, Autokey, Bacon, Bifid, Four-square, Monoalphabetic Substitution, Morse Code, Null, ROT-47, Running Key, Sandorf's, Shift (Caesar, ROT-13), Tap Code, Two-square, Vigenère
 
-### Ciphers with streaming support
+### Streaming support
 Affine, Atbash, Monoalphabetic Substitution, ROT-47, Running Key, Shift (Caesar, ROT-13), Vigenère
 
 ## Tools
-* Polybius Square 
+* Frequency Analysis
+* Polybius Square
 * Straddling Checkerboard
 * Tabula Recta
 
---
+++ Science.Cryptography.Ciphers/Analysis/FrequencyAnalysis.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Science.Cryptography.Ciphers.Analysis
+{
+    /// <summary>
+    /// Measures character frequencies.
+    /// </summary>
+    public static class FrequencyAnalysis
+    {
+        /// <summary>
+        /// Measures character frequencies in a given text.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static IReadOnlyDictionary<char, int> Analyze(string text)
+        {
+            if (text == null)
+                throw new ArgumentNullException("text");
+
+            return text.ToCharArray()
+                .GroupBy(c => c)
+                .ToDictionary(g => g.Key, g => g.Count())
+            ;
+        }
+    }
+}
