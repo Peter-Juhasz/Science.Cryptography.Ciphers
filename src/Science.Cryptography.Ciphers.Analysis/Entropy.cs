2679371b33d3794a0dafdca3dd122ea108a140c7
Peter-Juhasz <peter.juhasz@live.com>
2016-10-28 17:33:19 +0200

    XOR cipher and entropy

---
+++ src/Science.Cryptography.Ciphers.Analysis/Entropy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Science.Cryptography.Ciphers.Analysis
+{
+    public static class Entropy
+    {
+        /// <summary>
+        /// Calculates the entropy of <paramref name="input" />.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="logarithmBase"></param>
+        /// <returns></returns>
+        public static double Analyze(string input, double logarithmBase = 2)
+        {
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
+
+            return -(
+                from kv in FrequencyAnalysis.Analyze(input)
+                let occurrence = kv.Value
+                let probability = (double)occurrence / input.Length
+                select probability * Math.Log(probability, logarithmBase)
+            ).Sum();
+        }
+    }
+}
---
+++ src/Science.Cryptography.Ciphers/XorCipher.cs
@@ -0,0 +1,41 @@
+﻿using Science.Cryptography.Ciphers;
+using System;
+using System.Composition;
+using System.Linq;
+
+namespace ClassLibrary2
+{
+    /// <summary>
+    /// Represents the XOR cipher.
+    /// </summary>
+    [Export("XOR", typeof(IKeyedCipher<>))]
+    public class XorCipher : IKeyedCipher<byte[]>
+    {
+        public string Encrypt(string plaintext, byte[] key)
+        {
+            return Crypt(plaintext, key);
+        }
+
+        public string Decrypt(string ciphertext, byte[] key)
+        {
+            return Crypt(ciphertext, key);
+        }
+
+
+        internal static string Crypt(string text, byte[] key)
+        {
+            if (text == null)
+                throw new ArgumentNullException(nameof(text));
+
+            if (key == null)
+                throw new ArgumentNullException(nameof(key));
+
+            if (key.Length == 0)
+                throw new ArgumentException("Key can't be zero-length.", nameof(key));
+
+            return String.Concat(
+                text.Zip(EnumerableEx.Repeat<byte>(key), (c, k) => (char)(c ^ k))
+            );
+        }
+    }
+}
