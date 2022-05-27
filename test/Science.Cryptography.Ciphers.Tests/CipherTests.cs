b50e998269540df8a981fe6bdea68d6e7e0bfa9f
Juhász Péter <peter.juhasz@live.com>
2022-05-27 22:31:45 +0200

    polybius encrypt

--- test/Science.Cryptography.Ciphers.Tests/CipherTests.cs
+++ test/Science.Cryptography.Ciphers.Tests/CipherTests.cs
@@ -347,4 +347,17 @@ public class CipherTests
 		Assert.AreEqual(ciphertext, cipher.Encrypt(plaintext, square));
 		Assert.AreEqual(plaintext, cipher.Decrypt(ciphertext, square));
 	}
+
+	[TestMethod]
+	public void Polybius()
+	{
+		var cipher = new PolybiusCipher();
+
+		const string plaintext = "HELXLO WORLDX";
+		const string ciphertext = "343214522252 524332222455";
+		var square = PolybiusSquare.CreateFromKeyword("PLAYFAIR", WellKnownAlphabets.EnglishWithoutJ);
+
+		Assert.AreEqual(ciphertext, cipher.Encrypt(plaintext, square));
+		Assert.AreEqual(plaintext, cipher.Decrypt(ciphertext, square));
+	}
 }
